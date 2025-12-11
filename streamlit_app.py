d4e9cc92982947672015aaf94ff870a85e36ff52
mzikaadesta <m.zikaadesta@gmail.com>
2025-12-11 15:37:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,15 @@ import streamlit as st
 
 st.title("This is a title")
 st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+def pangkat(x, n):
+    if n == 0:
+        return 1
+    f
+    if n < 0:
+        return 1 / pangkat(x, -n)
+    return x * pangkat(x, n - 1)
+
+#  penggunaan
+print(pangkat(2, 5))   
+print(pangkat(3, 3))   
+print(pangkat(2, -3))   
