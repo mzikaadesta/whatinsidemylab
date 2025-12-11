10e340672ab05252e931c4dd6b45262aef7898be
mzikaadesta <m.zikaadesta@gmail.com>
2025-12-11 15:53:15 +0700

    Update Streamlit title and subtitle

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("This is a title")
-st.title("_Streamlit_ is :blue[cool] :sunglasses:")
+st.title("HAPPY BIRD DAYYY")
+st.title("_Streamlit_ is :blue[ziva🧁🧁] :sunglasses:")
 
